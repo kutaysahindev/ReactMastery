c1795911428a56c9819d90bae481cecb57e0e193
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-09-24 22:26:08 +0300

    Update App.js

--- 3-steps/src/App.js
+++ 3-steps/src/App.js
@@ -11,16 +11,16 @@ function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
-    <div>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
 
@@ -52,7 +52,7 @@ function App() {
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
