e81c9ad716ada550033d03927f9c78d461fea720
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-12-27 21:41:13 +0300

    Add esc key event handler

--- 6-usepopcorn/src/App.js
+++ 6-usepopcorn/src/App.js
@@ -109,6 +109,7 @@ export default function App() {
         setError("");
         return;
       }
+      handleCloseMovie();
       fetchMovies();
 
       return function () {
@@ -303,6 +304,20 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
 
   useEffect(
     function () {
