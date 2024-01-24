5bd8ad13fd0e58d1ddee5413d7d0b2021f4047be
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-01-24 23:50:06 +0300

    Fetch data w/JSON server

--- 8-react-quiz/src/App.js
+++ 8-react-quiz/src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 const App = () => {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
   return (
     <div className="app">
       <Header />
